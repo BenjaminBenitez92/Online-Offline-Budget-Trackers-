c6af1b4c5f74a7025df8499ff8f9dd13cfcaa5a4
BenjaminBenitez92 <81438491+BenjaminBenitez92@users.noreply.github.com>
2021-11-07 14:14:12 -0500

    depolying

--- Develop/server.js
+++ Develop/server.js
@@ -28,7 +28,7 @@ mongoose.connect("mongodb://localhost/budget", {
 // routes
 app.use(require("./routes/api.js"));
 router.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, "./public/index.html"))
+  res.sendFile(path.join(__dirname, "public/index.html"))
 });
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
